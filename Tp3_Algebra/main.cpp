735487ca35bc0eb54d897d351950995c02bb88fe
juandigilio <juanignaciodigilio@gmail.com>
2023-10-02 13:02:33 -0300

    .
    .

--- Tp3_Algebra/main.cpp
+++ Tp3_Algebra/main.cpp
@@ -1,51 +1,53 @@
 #include "raylib.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
+   
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
 
-    Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
+    Camera camera = { 0 };
+    camera.position = { 0.0f, 10.0f, 10.0f };
+    camera.target = { 0.0f, 0.0f, 0.0f };
+    camera.up = { 0.0f, 1.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+   
+    Model icosaedro = LoadModel("Obj/Icosahedron.obj");
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+
+    SetTargetFPS(60);         
+   
+    while (!WindowShouldClose())    
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
+       
+        /*if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
         if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
         if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
+        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;*/
+       
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
         DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
 
-        DrawCircleV(ballPosition, 50, MAROON);
+        BeginMode3D(camera);
+
+        DrawModel(icosaedro, {0,0,0}, 1, RED);
+
+        EndMode3D();
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
+       
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+   
+    CloseWindow(); 
+    
     return 0;
 }
