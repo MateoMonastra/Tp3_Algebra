f55e0e45c679d35456afdf5c24c1ef5cb74e3c3b
Mau <mauvepinktree@gmail.com>
2023-10-09 11:39:46 -0300

    Added dice model

--- Tp3_Algebra/main.cpp
+++ Tp3_Algebra/main.cpp
@@ -10,14 +10,14 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
 
     Camera camera = { 0 };
-    camera.position = { 0.0f, 10.0f, 10.0f };
-    camera.target = { 0.0f, 0.0f, 0.0f };
-    camera.up = { 0.0f, 1.0f, 0.0f };
+    camera.position = Vector3{ 0.0f, 5.0f, 20.0f };
+    camera.target = Vector3{ 0.0f, 5.0f, 0.0f };
+    camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
     camera.fovy = 45.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 
    
-    Model icosaedro = LoadModel("Obj/Icosahedron.obj");
+    Model dice = LoadModel("../res/3x3x3.obj");
     
     SetTargetFPS(60);         
    
@@ -29,16 +29,19 @@ int main(void)
         if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
         if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;*/
        
+        UpdateCamera(&camera, CAMERA_ORBITAL);
+
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
         DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
 
         BeginMode3D(camera);
 
-        DrawModel(icosaedro, {0,0,0}, 50, RED);
+        DrawGrid(20, 1);
 
+        DrawModel(dice, {0,0,0}, 5, RED);
         
 
         EndMode3D();
